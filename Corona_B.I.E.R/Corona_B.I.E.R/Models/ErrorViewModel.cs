403aca62eb6f4362a23931018cc4e989fffec26b
GijsVogels98 <44775226+GijsVogels98@users.noreply.github.com>
2020-04-02 14:36:51 +0200

    Basic Template

---
+++ Corona_B.I.E.R/Corona_B.I.E.R/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Corona_B.I.E.R.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
