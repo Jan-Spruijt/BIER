e074ee4cdf94681d3e60b1471a56b7ba7c1f7003
DavidRRg <57133584+DavidRRg@users.noreply.github.com>
2020-04-10 11:03:49 +0200

    Werknemer toevoegen en bekijken toegevoegd. Ook een datalayer toegevoegd in een apparte classlibrary. 10-4-2020

---
+++ Corona_B.I.E.R_V1/Corona_B.I.E.R_V1/Controllers/EmployeeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Corona_B.I.E.R_V1.DataLogic;
+using Corona_B.I.E.R_V1.Extensionmethods;
+using Corona_B.I.E.R_V1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Corona_B.I.E.R_V1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult RegisterEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult RegisterEmployee(EmployeeModel employee)
+        {
+            if (ModelState.IsValid)
+            {
+                EmployeeProcessor.CreateUser(
+                    employee.Firstname,
+                    employee.Prefix,
+                    employee.Lastname,
+                    employee.City,
+                    employee.Postalcode,
+                    employee.Address,
+                    employee.ProfilePicturePath,
+                    employee.Email,
+                    employee.Phone,
+                    employee.Password,
+                    employee.Profession,
+                    employee.Role.ToString()
+                );
+                return RedirectToAction("Index","Home");
+            }
+            return View();
+        }
+
+        public IActionResult ViewEmployees()
+        {
+            var data = EmployeeProcessor.LoadEmployees();
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+            foreach (var row in data)
+            {
+                employees.Add(new EmployeeModel
+                {
+
+                    Firstname = row.Firstname,
+                    Prefix = row.Lastnameprefix,
+                    Lastname = row.Lastname,
+                    City = row.City,
+                    Postalcode = row.PostalCode,
+                    Address = row.Address,
+                    ProfilePicturePath = row.ProfilePicturePath,
+                    Email = row.Email,
+                    Phone = row.Phone,
+                    Password = row.PasswordHash,
+                    Profession = row.Profession,
+                    Role = row.Role.ToEnum<EmployeeRole>()
+                });
+            }
+            return View(employees);
+        }
+    }
+}
---
+++ Corona_B.I.E.R_V1/Corona_B.I.E.R_V1/extensionmethods/EnumExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Corona_B.I.E.R_V1.Extensionmethods
+{
+    public static class EnumExtensionMethods
+    {
+        public static T ToEnum<T>(this string value, bool ignoreCase = true)
+        {
+            return (T)Enum.Parse(typeof(T), value, ignoreCase);
+        }
+    }
+}
