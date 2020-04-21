aa57725041007a2689da180569f9de7235d0f40e
Randy Barents <randybarents@gmail.com>
2020-04-21 12:10:51 +0200

    DateDataModel + proccessor

---
+++ Corona_B.I.E.R_V1/DataLayerLibrary/DataLogic/EventProcessor.cs
@@ -0,0 +1,41 @@
+﻿using Corona_B.I.E.R_V1.DataAccess;
+using Corona_B.I.E.R_V1.DataModels;
+using Corona_B.I.E.R_V1;
+using System.Collections.Generic;
+using Corona_B.I.E.R_V1.Models;
+using System.Security.Principal;
+
+namespace DataLayerLibrary.DataLogic
+{
+    public class EventProcessor
+    {
+        public static void CreateEvent(int id , string title , string description , string type , string start , string end)
+        {
+            DateDataModel data = new DateDataModel
+            {
+                Id = id,
+                Title = title,
+                Description = description,
+                Type = type,
+                Start = start,
+                End = end
+            };
+
+            string sql = @"INSERT INTO schedule (id, datetimeStart, datetimeENd, title, description, type)
+                            VALUES(@Id , @Start , @End , @Title , @Decription , @Type);";
+            SQLDataAccess.SaveData(sql, data);
+        }
+
+        public static List<DateDataModel> LoadDates()
+        {
+            string sql = "SELECT * FROM schedule;";
+            return SQLDataAccess.LoadData<DateDataModel>(sql);
+        }
+
+        public static DateDataModel getDateByType(string type)
+        {
+            string sql = "SELECT * FROM schedule WHERE type = '{type}';";
+            return SQLDataAccess.LoadFirstData<DateDataModel>(sql);
+        }
+    }
+}
---
+++ Corona_B.I.E.R_V1/DataLayerLibrary/DataModels/DateDataModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace Corona_B.I.E.R_V1.Models
+{
+    public class DateDataModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Start { get; set; }
+        public string End { get; set; }
+        public string Type {get; set;}
+    }
+}
