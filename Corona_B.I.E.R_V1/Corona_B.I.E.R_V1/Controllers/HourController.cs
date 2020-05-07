6dc5f9bb5bfb2efbc93c55a950f11d1946914cde
Maik <57899258+MaikGeraets@users.noreply.github.com>
2020-05-07 15:23:31 +0200

    update

---
+++ Corona_B.I.E.R_V1/Corona_B.I.E.R_V1/Controllers/HourController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Corona_B.I.E.R_V1.Models;
+
+namespace Corona_B.I.E.R_V1.Controllers
+{
+
+
+    public class HourController : ControllerBase
+    {
+
+        // GET: api/Hour
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Hour/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Hour
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Hour/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
