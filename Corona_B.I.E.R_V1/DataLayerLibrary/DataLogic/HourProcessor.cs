0964f32ecaed84d702a7f41c7fad4d64f3efcf7e
Maik <57899258+MaikGeraets@users.noreply.github.com>
2020-05-14 23:10:03 +0200

    query

---
+++ Corona_B.I.E.R_V1/DataLayerLibrary/DataLogic/HourProcessor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Corona_B.I.E.R_V1.DataAccess;
+using Corona_B.I.E.R_V1.DataModels;
+using Corona_B.I.E.R_V1;
+using DataLayerLibrary.DataModels;
+using Org.BouncyCastle.Asn1.Cms;
+
+namespace DataLayerLibrary.DataLogic
+{
+    class HourProcessor
+    {
+
+        public static void CreateHours(string employee_id, string standbyHours, string incidentHours, string timestamp)
+        {
+            HourDataModel data = new HourDataModel
+            {
+
+                Employee_Id = employee_id,
+                StandbyHours = standbyHours,
+                IncidentHours = incidentHours,
+                TimeStamp = timestamp,
+
+
+    };
+            string sql = @"INSERT INTO registered-hours (employee_id, standbyHours, incidentHours, timestamp )
+                            VALUES(@Employee_Id, @StandbyHours, @IncidentHours, @TimeStamp);";
+            SQLDataAccess.SaveData(sql, data);
+        }
+    }
+}
---
+++ Corona_B.I.E.R_V1/DataLayerLibrary/DataModels/HourDataModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayerLibrary.DataModels
+{
+    class HourDataModel
+    {
+        public int ID { get; set; }
+        public string Employee_Id { get; set; }
+        public string StandbyHours { get; set; }
+        public string IncidentHours { get; set; }
+        public string TimeStamp { get; set; }
+
+    }
+}
