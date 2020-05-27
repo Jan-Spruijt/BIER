80d9d8ff44dda64fd869dc68b1a8cd4bd46a9308
GijsVogels98 <44775226+GijsVogels98@users.noreply.github.com>
2020-05-27 20:56:19 +0200

    /

---
+++ Corona_B.I.E.R_V1/Corona_B.I.E.R_V1/ViewComponents/RequestDashboardViewComponent.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Corona_B.I.E.R_V1.DataLogic;
+using Corona_B.I.E.R_V1.DataModels;
+using Corona_B.I.E.R_V1.ExtensionMethods;
+using Corona_B.I.E.R_V1.Models;
+using DataLayerLibrary.DataLogic;
+using LogicLayerLibrary.ExtensionMethods;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Corona_B.I.E.R_V1.ViewComponents
+{
+    public class RequestDashboardViewComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var data = RequestProcessor.LoadRequests();
+            List<RequestModel> requests = new List<RequestModel>();
+            foreach (var row in data)
+            {
+                EmployeeDataModel employee = EmployeeProcessor.GetUserById(row.employee_id);
+
+                EmployeeDataModel? AcceptedByEmployee = EmployeeProcessor.GetUserById(row.employee_id_acceptedby);
+                string nameAccept = "Nog niet geaccepteerd";
+                if (AcceptedByEmployee != null)
+                {
+                    nameAccept = AcceptedByEmployee.Firstname + " " + AcceptedByEmployee.Lastname;
+                }
+
+                string name = employee.Firstname + " " + employee.Lastname;
+                string dateStart = row.datetimestart.ToShortDateString();
+                string dateEnd = row.datetimeend.ToShortDateString();
+                string TimeStart = row.datetimestart.ToShortTimeString();
+                string TimeEnd = row.datetimeend.ToShortTimeString();
+
+                requests.Add(new RequestModel
+                {
+                    RequestId = row.id,
+                    Employeename = name,
+                    EmployeeAcceptedbyName = nameAccept,
+                    stringDateStart = dateStart,
+                    stringDateEnd = dateEnd,
+                    stringTimeStart = TimeStart,
+                    stringTimeEnd = TimeEnd,
+                    Status = row.status.ToEnum<RequestStatus>(),
+                    Type = row.request_type.ToEnum<RequestType>()
+                });
+            }
+
+
+            return View(requests);
+        }
+
+    }
+}
